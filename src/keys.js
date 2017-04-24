9e9dac6142ee45069ea4c089cf3b096652f3dc8c
Michael <mcl282@gmail.com>
2017-04-24 01:44:55 +0000

    took keys out of gitignore file

---
+++ src/keys.js
@@ -0,0 +1,4 @@
+//http://stackoverflow.com/questions/40756359/uncaught-syntaxerror-unexpected-token-export
+export const googleApiKey = 'AIzaSyAnhYQqHo2V5AcFpcKKPX6rz0bVrw7xmZg';
+export const zillowZWSID = 'X1-ZWz198ptrlda17_6ttia'; //Zillow Web Services Identification (ZWSID) 
+
