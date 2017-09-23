b86f8b82c3b7e3db822631735a1fee603bcc00de
Michael <mcl282@gmail.com>
2017-09-23 04:34:29 +0000

    remove extra character in stripe action

--- src/actions/stripe_actions.js
+++ src/actions/stripe_actions.js
@@ -4,7 +4,7 @@ import { ROOT_URL_VERSION } from './index.js';
 
 const jwt = localStorage.getItem('jwt');
 
-const auth_header = { 'Authorization': `Bearer + ${jwt}`};
+const auth_header = { 'Authorization': `Bearer ${jwt}`};
 
 
 export const createStripeCharge = (stripeToken, amount, callback) => {
@@ -16,7 +16,7 @@ export const createStripeCharge = (stripeToken, amount, callback) => {
     data: { stripeToken, amount }, 
     headers: auth_header })
     .then(response => {
-      console.log(response)
+      console.log(response);
       dispatch({ 
         type: CREATE_STRIPE_CHARGE,
         payload: response
@@ -26,6 +26,7 @@ export const createStripeCharge = (stripeToken, amount, callback) => {
     .catch( error => {
     //if request is bad, show an error to the user
     console.log(error.response);
+    console.log(auth_header);
     dispatch({
       type: STRIPE_CHARGE_ERROR,
       payload: error.response.data
